3582bd7d3399b7cbf55dc9e6d3be8ae5f1a1410d
Julio Sergio <juliosergio010302@gmail.com>
2021-06-16 09:44:54 -0300

    Wrote a little story

--- Giraffe/Sources/main.c
+++ Giraffe/Sources/main.c
@@ -3,9 +3,10 @@
 
 int main()
 {
-    printf("   /|\n");
-    printf("  / |\n");
-    printf(" /  |\n");
-    printf("/___|\n");
+    printf("There once was a man named George\n");
+    printf("he was 70 years old.\n");
+    printf("He really liked the name George\n");
+    printf("but did not like being 70.\n");
+
     return 0;
 }
