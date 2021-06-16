4c2c369876a4d0989066542b5fa8f2c1b3460de1
Julio Sergio <juliosergio010302@gmail.com>
2021-06-16 11:21:40 -0300

    Used user input

--- Giraffe/Sources/main.c
+++ Giraffe/Sources/main.c
@@ -4,11 +4,35 @@
 int main()
 {
 
-    /* usually const values have their name in uppercase */
-    const int NUMBER = 10;
-    /* this is also technically a const: */
-    printf("Hello");
-    /* because the value inside the printf cannot be modified*/
+    /*int age;
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    printf("You are %d years old\n", age);
+    printf("\n");
+
+    double gpa;
+    printf("Enter your gpa: ");
+    scanf("%lf", &gpa);
+    printf("Your gpa is %f\n", gpa);
+    printf("\n");
+    
+    char grade;
+    printf("Enter your grade: ");
+    scanf("%c", &grade);
+    printf("Your grade is %c\n", grade);
+    printf("\n");
+
+    char name[20];
+    printf("Enter your name: ");
+    scanf("%s", name);
+    printf("Your name is %s.\n", name);
+    printf("\n");*/
+
+    char fullName[20];
+    printf("Enter your name: ");
+    fgets(fullName, 20, stdin);
+    printf("Your name is %s.\n", fullName);
+    printf("\n");
 
     return 0;
 }
